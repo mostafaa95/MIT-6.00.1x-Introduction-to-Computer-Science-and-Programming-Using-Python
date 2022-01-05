0c3c2b86fd617808ff53c20b10ed388b97b64bfe
mostafaa95 <68716880+mostafaa95@users.noreply.github.com>
2022-01-05 02:59:17 +0200

    Update problem.py

--- pset1/problem.py
+++ pset1/problem.py
@@ -1,1 +1,13 @@
 
+# -*- coding: utf-8 -*-
+# Write a program that counts up the number of vowels contained in the string s. 
+# Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. 
+# For example, if s = 'azcbobobegghakl', your program should print:
+s = 'azcbobobegghakl'
+count = 0
+z = -1
+for k in s:
+    z+=1
+    if (s[z] == 'a' or s[z] == 'e' or s[z] == 'i'or s[z] == 'o'or s[z] == 'u'):
+        count+=1
+print('Number of vowels:',count)
