c6099c6eb51477e67a0eed170d8afddf5a81f003
mostafaa95 <68716880+mostafaa95@users.noreply.github.com>
2022-01-05 16:05:34 +0200

    Create Problem 3 - Longest Alphabetcal Substring.py

---
+++ pset1/Problem 3 - Longest Alphabetcal Substring.py
@@ -0,0 +1,33 @@
+# Assume s is a string of lower case characters.
+
+# Write a program that prints the longest substring of s in which the letters 
+# occur in alphabetical order. For example, if s = 'azcbobobegghakl', 
+# then your program should print
+
+# Longest substring in alphabetical order is: beggh
+
+# In the case of ties, print the first substring. For example, if s = 'abcbcd', 
+# then your program should print
+
+# Longest substring in alphabetical order is: abc
+
+# Note: This problem may be challenging. We encourage you to work smart. 
+# If you'\ve spent more than a few hours on this problem, we suggest 
+# that you move on to a different part of the course. If you have time, 
+# come back to this problem after you've had a break and cleared your head.
+
+# Paste your code into this box
+lala = 0
+maxcount = 0
+result = 0
+
+for char in range(len(s) - 1):
+    if (s[char] <= s[char + 1]):
+        lala+= 1
+        if lala > maxcount:
+            maxcount = lala
+            result = char + 1
+    else:
+        lala = 0
+startposition = result - maxcount
+print('Longest substring in alphabetical order is:', s[startposition:result + 1])
